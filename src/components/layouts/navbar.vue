cae2e7e771cc21240bac563b76ef136c09ba1ae9
Dusan Vostic <vosticdusan@gmail.com>
2020-10-25 17:49:26 +0100

    task num #1 - Added vue router and bootstrap. Created layouts folder in src/components with navbar file. Created new component-view AppMovies with route /movies and setted as default route of project.

---
+++ src/components/layouts/navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div id="nav" class="navbar navbar-brand">
+      <router-link to="/" class="navbar-brand">Home</router-link> |
+      <router-link to="/about" class="navbar-brand">About</router-link> |
+      <router-link to="/movies" class="navbar-brand">Movies</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "navbar"
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/AppMovies.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    App Movies Views
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppMovies"
+}
+</script>
+
+<style scoped>
+
+</style>
