aa3cdc7eef5a1088ec9bd42bccca179ee723b63e
Dusan Vostic <vosticdusan@gmail.com>
2020-10-26 12:31:38 +0100

    task num #6 - In AppMovies component/view added error  message if is there no movie to show.

--- src/views/AppMovies.vue
+++ src/views/AppMovies.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="card-deck">
+<div>
+  <div v-if="!filteredMovies.length" class="alert alert-danger" role="alert">
+    <p>Sorry!</p>
+    <p>We don't have any movie with that search input.</p>
+    <p>Please, try something else. Thanks!</p>
+  </div>
+  <div v-else class="card-deck">
     <movie-row v-for="movie in filteredMovies" :key="movie.id" :movie=movie />
   </div>
+
+</div>
 </template>
 
 <script>
