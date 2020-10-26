72168be2df45365ff0ba99b8c7b03c2fc8b619f4
Dusan Vostic <vosticdusan@gmail.com>
2020-10-26 13:22:08 +0100

    task num #7 update - In AppMovie view added count for selected movies.

--- src/views/AppMovies.vue
+++ src/views/AppMovies.vue
@@ -1,13 +1,20 @@
 <template>
 <div>
-  <div v-if="!filteredMovies.length" class="alert alert-danger" role="alert">
+  <div v-if="filteredMovies.length">
+    <div>
+      <span>Number of selected movies: {{ selectedMovies.length }}</span>
+    </div>
+    <div class="card-deck">
+      <movie-row v-for="movie in filteredMovies" :key="movie.id" :movie=movie :isSelected="isMovieSelected(movie)" @movie-selected="handleMovieSelected"/>
+    </div>
+  </div>
+
+  <div  v-else  class="alert alert-danger" role="alert">
     <p>Sorry!</p>
     <p>We don't have any movie with that search input.</p>
     <p>Please, try something else. Thanks!</p>
   </div>
-  <div v-else class="card-deck">
-    <movie-row v-for="movie in filteredMovies" :key="movie.id" :movie=movie :isSelected="isMovieSelected(movie)" @movie-selected="handleMovieSelected"/>
-  </div>
+  
 
 </div>
 </template>
@@ -39,7 +46,6 @@ export default {
   methods: {
     isMovieSelected(movie) {
       const value = (this.selectedMovies.includes(movie) ? true : false);
-      console.log(value);
       return value;
     },
 
