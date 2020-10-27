ada6ceb5a50b75edae15f7213d8bd446fe469744
Dusan Vostic <vosticdusan@gmail.com>
2020-10-27 21:19:27 +0100

    laravel/vue task #2 - In moviesServices changed base apiClint url, so now we use our api from movie-api project.

--- src/services/moviesServices.js
+++ src/services/moviesServices.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default class MoviesServices {
   constructor() {
     this.apiClient = axios.create({
-      baseURL: 'http://localhost:3000/api',
+      baseURL: 'http://localhost:8000/api',
       timeout: 1000,
     })
   }
