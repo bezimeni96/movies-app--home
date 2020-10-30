96cafb61cf90cb2280783c1165e57e9993f3849b
Dusan Vostic <vosticdusan@gmail.com>
2020-10-30 21:04:03 +0100

    laravel/vue task #10

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import AppMovies from '../views/AppMovies.vue';
 import AddMovie from '../views/AddMovie';
 import AppLogin from '../views/AppLogin';
 import AppRegister from '../views/AppRegister';
+import { store } from '../store/movieStore';
 
 Vue.use(VueRouter)
 
@@ -21,17 +22,20 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    meta: { isAuthRequired: true}
   },
   {
     path: '/movies',
     name: 'movies',
-    component: AppMovies
+    component: AppMovies,
+    meta: { isAuthRequired: true}
   },
   {
     path: '/add',
     name: 'add',
-    component: AddMovie
+    component: AddMovie,
+    meta: { isAuthRequired: true}
   },
   {
     path: '/login',
@@ -43,12 +47,28 @@ const routes = [
     name: 'register',
     component: AppRegister
   }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  if (!to.meta.isAuthRequired  && !localStorage.getItem('token')) {
+    next();
+  } else {
+    if (!store.getToken && localStorage.getItem('token')) {
+      store.commit('setToken', localStorage.getItem('token'));
+      next();
+    } else  {
+      if (!localStorage.getItem('token')) {
+        next('/login');
+      }
+      next();
+    } 
+  }
 })
 
 export default router
